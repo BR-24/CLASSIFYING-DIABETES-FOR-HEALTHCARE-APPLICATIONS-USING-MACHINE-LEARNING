1fc2aa03a0e00985a9b28fcb8c30e33bbdf5b395
BR-24 <130635858+BR-24@users.noreply.github.com>
2026-06-01 12:12:36 +0530

    TestSingleInput.py

--- TestSingleInput.py
+++ TestSingleInput.py
@@ -4,14 +4,13 @@ from joblib import load
 from tensorflow.keras.models import load_model
 import warnings
 
-# Ignore all warnings
 warnings.filterwarnings("ignore")
 
-# Load pre-trained models
+
 with open('ann_model.h5', 'rb') as f:
     ann_model = load_model('ann_model.h5')
 
-# Function to get user input
+
 def get_user_input():
     print("Enter the following parameters for diabetes prediction:")
     pregnancies = int(input("Number of pregnancies: "))
@@ -25,15 +24,14 @@ def get_user_input():
 
     return np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]])
 
-# Function to display prediction results (Yes/No)
+
 def display_result(prediction):
     return "Yes" if prediction == 1 else "No"
 
-# Main function to get input and make predictions
+
 def predict_diabetes():
     user_input = get_user_input()
 
-    # Predict using ANN model
     ann_prediction = ann_model.predict(user_input)
     print(f"ANN Model Prediction: {display_result(ann_prediction[0])}")
 
